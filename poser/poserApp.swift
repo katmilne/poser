95b26060dff64887f75cb86279aad8f54ad04595
katmilne <katmilne39@gmail.com>
2026-07-14 23:08:34 -0700

    Initial Commit

---
+++ poser/poserApp.swift
@@ -0,0 +1,17 @@
+//
+//  poserApp.swift
+//  poser
+//
+//  Created by Kat on 2026-07-14.
+//
+
+import SwiftUI
+
+@main
+struct poserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
